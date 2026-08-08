a3ec1d78b3d9137dfc50ac3ff904e2c9c14c6b67
Yeshiwas <yeshiwasdagnaw23@gmail.com>
2026-08-08 18:50:26 +0300

    Tigregna translation added, ad allUsers page translated

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -4,12 +4,15 @@ import { initReactI18next } from 'react-i18next';
 import en from '../locales/en.json';
 import am from '../locales/am.json';
 import om from '../locales/om.json';
+import tg from '../locales/tg.json'
 
 i18n.use(initReactI18next).init({
     resources: {
         en: { translation: en },
         am: { translation: am },
         om: { translation: om },
+        tg: { translation: tg },
+
     },
     lng: localStorage.getItem('siralink_language') || 'en',
     fallbackLng: 'en',
